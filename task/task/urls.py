12554e5fb0cd113a1f98834d59118b55664182d0
bharish1997 <bharish28081997@gmail.com>
2021-01-09 13:42:09 +0530

    harish status code

--- task/task/urls.py
+++ task/task/urls.py
@@ -1,7 +1,7 @@
 """task URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,16 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import sys
-
 sys.path.append("..")
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+from taskapp import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-# router.register(r'users', views)
+# router.register(r'ping/', views.ping_view, basename="ping")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('ping/', views.ping_view)
 ]
--- task/taskapp/views.py
+++ task/taskapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def ping_view(request):
+    if request.method == 'GET':
+        return Response(status=200)
