16c4301446c08cc8ea92506929761b500beb01c9
bharish1997 <bharish28081997@gmail.com>
2021-01-09 18:09:35 +0530

    bharish final

--- task/taskapp/views.py
+++ task/taskapp/views.py
@@ -2,20 +2,19 @@ from django.shortcuts import render
 import requests as r
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import datetime
+from datetime import datetime
 
 
 # Create your views here.
 @api_view(['GET'])
 def ping_view(request):
-    print(datetime.date.today())
     if request.method == 'GET':
         return Response({"status": "OK"}, status=200)
 
 
 @api_view(['GET'])
 def first(request):
-    if str(request.GET.get('dt')) == str(datetime.date.today()):
+    if str(request.GET.get('dt')) == str(datetime.now().replace(second=0, microsecond=0)):
         re = r.get(request.GET.get('url'))
         return Response(status=re.status_code)
     else:
